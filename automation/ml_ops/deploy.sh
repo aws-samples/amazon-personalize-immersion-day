51b8819a180b07f45a4856bc4091b8fd19c4bbbc
Alexandria Burkleaux <burkleaa@amazon.com>
2022-10-25 09:57:16 -0700

    Log output from scripts, get data for pre-trained

--- automation/ml_ops/deploy.sh
+++ automation/ml_ops/deploy.sh
@@ -39,14 +39,23 @@ unzip ml-latest-small.zip
 
 echo "Local copy sync Media-Pretrained"
 mkdir poc_data/imdb
-!aws s3 cp s3://aim312data/items.csv ./poc_data/imdb/items.csv
+aws s3 cp s3://aim312data/items.csv poc_data/imdb/items.csv
+mkdir domain/Media-Pretrained/data/
+mkdir domain/Media-Pretrained/data/Interactions/
+mkdir domain/Media-Pretrained/data/Items/
+mkdir domain/Media-Pretrained/data/Users/
+aws s3 cp s3://aim312data/items.csv domain/Media-Pretrained/data/Items/
+aws s3 cp s3://aim312data/users.csv domain/Media-Pretrained/data/Users/
+
+# Movie Lens data is imported into poc_data for the Media above
 
 cd ..
 pwd
 if [ "$2" == "Media-Pretrained" ]
 then
     echo "Preprocess the IMDB and Movielens data"
-    python script-Pretrained.py
+    python script-Pretrained.py >script-Pretrained.out 2>&1
+    
 else
     echo "Preprocess the Movielens data"
     python script.py
@@ -76,8 +85,9 @@ then
     aws s3 cp ./domain/$2/params.json s3://$bucket 
 elif [ "$2" = "Media-Pretrained" ]
 then
-    aws s3 cp s3://aim312data/items.csv s3://$bucket/Items/items.csv
     echo "Starting the copy to S3 Media data"
-    aws s3 cp ./domain/$2/data/Interactions/interactions.csv s3://$bucket/Interactions/interactions.csv
+    aws s3 cp ./poc_data/users.csv s3://$bucket/Users/users.csv
+    aws s3 cp ./poc_data/interactions.csv s3://$bucket/Interactions/interactions.csv
+    aws s3 cp ./poc_data/items.csv s3://$bucket/Items/items.csv
     aws s3 cp ./domain/$2/params.json s3://$bucket 
 fi
