9157c51881e9e0f730481623dea9a8d9d4554dc7
aburkleaux-amazon <33331299+aburkleaux-amazon@users.noreply.github.com>
2022-10-25 18:33:16 -0700

    Add automated deployment for the 2 hour workshop with pre-trained models (#32)
    Automated deployment of the new Media-Pretrained workshop.
    
    1. Update the Maintaining Personalized Experiences with Machine Learning stack used by the workshop to the latest version to support Use Case Optimized Recommenders.
    2. Change the Personalize resources deployed to match the flow of the new workshop version.
    3. Import notebooks and data for the 2 hour workshop in Media-Pretrained folders
    4. Import new items data from S3 bucket
    5. Update the Movie Lens and Items data preprocessing to match the steps in 01_Data_Layer.ipynb
    6. Update .gitignore to avoid pulling in hidden files that may be lying around from Jupyter

--- automation/ml_ops/deploy.sh
+++ automation/ml_ops/deploy.sh
@@ -4,8 +4,15 @@ sudo usermod -a -G docker ec2-user
 docker ps
 pip install aws-sam-cli
 sam --version
-sam deploy --template-file template.yaml --stack-name id-ml-ops --capabilities CAPABILITY_IAM --s3-bucket $1
-bucket=$(aws cloudformation describe-stacks --stack-name id-ml-ops --query "Stacks[0].Outputs[?OutputKey=='InputBucketName'].OutputValue" --output text)
+if [ "$2" == "Media-Pretrained" ]
+then
+    sam deploy --template-file template_v1_2_0.template --stack-name id-ml-ops --capabilities CAPABILITY_IAM --s3-bucket $1
+else
+    sam deploy --template-file template.yaml --stack-name id-ml-ops --capabilities CAPABILITY_IAM --s3-bucket $1
+fi
+
+bucket=$(aws cloudformation describe-stacks --stack-name id-ml-ops --query "Stacks[0].Outputs[?OutputKey=='PersonalizeBucketName'].OutputValue" --output text)
+echo "Bucket is $bucket"
 echo "Local copy sync Retail"
 aws s3 cp s3://retail-demo-store-us-east-1/csvs/interactions.csv ./domain/Retail/data/Interactions/interactions.csv
 aws s3 cp s3://retail-demo-store-us-east-1/csvs/users.csv ./domain/Retail/data/Users/users.csv
@@ -22,6 +29,7 @@ aws s3 cp s3://personalization-at-amazon/personalize-immersion-day/CPG/Metadata/
 aws s3 cp s3://personalization-at-amazon/personalize-immersion-day/CPG/Metadata/items-origin.csv ./domain/CPG/data/metadata/items-origin.csv
 
 echo "Local copy sync Media"
+
 mkdir poc_data
 mkdir domain/Media/data/
 mkdir domain/Media/data/Interactions/
@@ -29,8 +37,33 @@ mkdir domain/Media/data/Items/
 cd poc_data 
 wget http://files.grouplens.org/datasets/movielens/ml-latest-small.zip
 unzip ml-latest-small.zip
+
+echo "Local copy sync Media-Pretrained"
+mkdir imdb
+
+echo "IMDB data setup"
 cd ..
-python script.py
+aws s3 cp s3://elementalrodeo99-us-west-1/aim312/items.csv poc_data/imdb/items.csv
+mkdir domain/Media-Pretrained/data/
+mkdir domain/Media-Pretrained/data/Interactions/
+mkdir domain/Media-Pretrained/data/Items/
+mkdir domain/Media-Pretrained/data/Users/
+aws s3 cp s3://elementalrodeo99-us-west-1/aim312/items.csv domain/Media-Pretrained/data/Items/
+aws s3 cp s3://elementalrodeo99-us-west-1/aim312/users.csv domain/Media-Pretrained/data/Users/
+
+# Movie Lens data is imported into poc_data for the Media above
+
+pwd
+if [ "$2" == "Media-Pretrained" ]
+then
+    echo "Preprocess the IMDB and Movielens data"
+
+    python script-Pretrained.py >script-Pretrained.out 2>&1
+    
+else
+    echo "Preprocess the Movielens data"
+    python script.py
+fi
 
 sleep 60
 
@@ -54,5 +87,11 @@ then
     aws s3 cp s3://personalization-at-amazon/personalize-immersion-day/CPG/Interactions/interactions.csv s3://$bucket/Interactions/interactions.csv
     aws s3 cp s3://personalization-at-amazon/personalize-immersion-day/CPG/Users/users.csv s3://$bucket/Users/users.csv
     aws s3 cp ./domain/$2/params.json s3://$bucket 
+elif [ "$2" = "Media-Pretrained" ]
+then
+    echo "Starting the copy to S3 Media data"
+    aws s3 cp ./poc_data/users.csv s3://$bucket/train/users.csv
+    aws s3 cp ./poc_data/interactions.csv s3://$bucket/train/interactions.csv
+    aws s3 cp ./poc_data/items.csv s3://$bucket/train/items.csv
+    aws s3 cp ./domain/$2/params.json s3://$bucket/train/params.json
 fi
-
