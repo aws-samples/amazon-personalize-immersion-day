4ad3c00398251e6c7589b0dafcff6450ba1e6611
aburkleaux-amazon <33331299+aburkleaux-amazon@users.noreply.github.com>
2022-11-14 19:46:30 -0800

    Update automation for promotions (#47)
    Update automated deployment data preparation to reflect changes to 01_Data_Layer.ipynb to support promotions

--- automation/ml_ops/deploy.sh
+++ automation/ml_ops/deploy.sh
@@ -58,7 +58,7 @@ if [ "$2" == "Media-Pretrained" ]
 then
     echo "Preprocess the IMDB and Movielens data"
 
-    python script-Pretrained.py >script-Pretrained.out 2>&1
+    ipython script-Pretrained.py >script-Pretrained.out 2>&1
     
 else
     echo "Preprocess the Movielens data"
@@ -92,6 +92,6 @@ then
     echo "Starting the copy to S3 Media data"
     aws s3 cp ./poc_data/users.csv s3://$bucket/train/users.csv
     aws s3 cp ./poc_data/interactions.csv s3://$bucket/train/interactions.csv
-    aws s3 cp ./poc_data/items.csv s3://$bucket/train/items.csv
+    aws s3 cp ./poc_data/item-meta.csv s3://$bucket/train/items.csv
     aws s3 cp ./domain/$2/params.json s3://$bucket/train/params.json
 fi
