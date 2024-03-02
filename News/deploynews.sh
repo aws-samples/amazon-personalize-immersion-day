66d1a4737e60a1403b363f4e87f788ec3980cea5
ewbolme <104103945+ewbolme@users.noreply.github.com>
2024-03-01 20:05:36 -0500

    Initial attempt to merge news

---
+++ News/deploynews.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+bucket=$1
+echo "Bucket is $bucket"
+echo "domain is $2"
+
+echo "Local copy sync CI&T Deskdrop News Dataset"
+#copy stuff here
+
+mkdir poc_data
+
+aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-news-immersion-day/deskdrop_articles_automated.csv ./poc_data/deskdrop_articles_automated.csv 
+aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-news-immersion-day/deskdrop_interactions_automated.csv ./poc_data/deskdrop_interactions_automated.csv 
+
+aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-news-immersion-day/shared_articles.csv ./poc_data/shared_articles.csv
+aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-news-immersion-day/users_interactions.csv ./poc_data/user_interactions.csv
+
+sleep 60
+echo "Starting the copy to S3 News Data"
+aws s3 cp ./poc_data/deskdrop_interactions_automated.csv s3://$bucket/train/interactions.csv
+aws s3 cp ./poc_data/deskdrop_articles_automated.csv s3://$bucket/train/items.csv
+aws s3 cp ./poc_data/params.json s3://$bucket/train/params.json
