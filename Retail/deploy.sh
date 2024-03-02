14d2f2b78a507cffdd340bc36eb3e7f75ad3169c
ewbolme <104103945+ewbolme@users.noreply.github.com>
2024-03-01 22:23:37 -0500

    Changes to retail section to account for recent move of data files

--- Retail/deploy.sh
+++ Retail/deploy.sh
@@ -5,16 +5,22 @@ echo "Bucket is $bucket"
 
 pwd
 
+mkdir poc_data
+mkdir poc_data/data
+mkdir poc_data/metadata
 
+echo "Local copy sync Retail"
+
+wget -P poc_data/data https://code.retaildemostore.retail.aws.dev/csvs/interactions.csv
+wget -P poc_data/data https://code.retaildemostore.retail.aws.dev/csvs/items.csv
+wget -P poc_data/data https://code.retaildemostore.retail.aws.dev/csvs/users.csv
+
+sleep 60
 echo "Starting the copy to S3 data"
-# aws s3 cp ./poc_data/users.csv s3://$bucket/train/users.csv
-# aws s3 cp ./poc_data/interactions.csv s3://$bucket/train/interactions.csv
-# aws s3 cp ./poc_data/items.csv s3://$bucket/train/items.csv
 
-
-aws s3 cp s3://retail-demo-store-us-east-1/csvs/users.csv s3://$bucket/train/users.csv
-aws s3 cp s3://retail-demo-store-us-east-1/csvs/interactions.csv s3://$bucket/train/interactions.csv
-aws s3 cp s3://retail-demo-store-us-east-1/csvs/items.csv s3://$bucket/train/items.csv
+aws s3 cp ./poc_data/data/users.csv s3://$bucket/train/users.csv
+aws s3 cp ./poc_data/data/interactions.csv s3://$bucket/train/interactions.csv
+aws s3 cp ./poc_data/data/items.csv s3://$bucket/train/items.csv
 
 sleep 60
 
