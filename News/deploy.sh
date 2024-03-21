25f96bc9252e16f6cab38b4f0d8b22e5d84c9f55
ewbolme <ericwbolme@gmail.com>
2024-03-21 21:34:38 +0000

    Update News Automation Script
    Update the News Automation Script to refer to CloudFront distribution

--- News/deploy.sh
+++ News/deploy.sh
@@ -8,14 +8,13 @@ echo "Local copy sync CI&T Deskdrop News Dataset"
 #copy stuff here
 
 mkdir poc_data
+sleep 30
 
-aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-news-immersion-day/deskdrop_articles_automated.csv ./poc_data/deskdrop_articles_automated.csv 
-aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-news-immersion-day/deskdrop_interactions_automated.csv ./poc_data/deskdrop_interactions_automated.csv 
+wget -P poc_data https://d2peeor3oplhc6.cloudfront.net/personalize-news-immersion-day/deskdrop_interactions_automated.csv
+wget -P poc_data https://d2peeor3oplhc6.cloudfront.net/personalize-news-immersion-day/deskdrop_articles_automated.csv
 
-aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-news-immersion-day/shared_articles.csv ./poc_data/shared_articles.csv
-aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-news-immersion-day/users_interactions.csv ./poc_data/user_interactions.csv
+sleep 30
 
-sleep 60
 echo "Starting the copy to S3 News Data"
 aws s3 cp ./poc_data/deskdrop_interactions_automated.csv s3://$bucket/train/news/interactions.csv
 aws s3 cp ./poc_data/deskdrop_articles_automated.csv s3://$bucket/train/news/items.csv
