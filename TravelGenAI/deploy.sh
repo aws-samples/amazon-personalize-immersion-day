b5e291e0a70c1734543a8cebfff08f2df8e36287
ewbolme <ericwbolme@gmail.com>
2024-08-02 01:26:37 +0000

    modifying deploy script to use cloudfront

--- TravelGenAI/deploy.sh
+++ TravelGenAI/deploy.sh
@@ -9,14 +9,13 @@ echo "Local copy sync Travel data"
 mkdir poc_data
 sleep 30
 
-# need to fix this to point to cloudfront
-aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-immersionday-travel/travel_interactions.csv ./poc_data/travel_interactions.csv
-aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-immersionday-travel/travel_items.csv ./poc_data/travel_items.csv
-aws s3 cp s3://personalize-solution-staging-us-east-1/personalize-immersionday-travel/travel_users.csv ./poc_data/travel_users.csv
+wget -P poc_data https://d2peeor3oplhc6.cloudfront.net/personalize-immersionday-travel/travel_users.csv  
+wget -P poc_data https://d2peeor3oplhc6.cloudfront.net/personalize-immersionday-travel/travel_items.csv  
+wget -P poc_data https://d2peeor3oplhc6.cloudfront.net/personalize-immersionday-travel/travel_interactions.csv  
 
 sleep 30
 
-echo "Starting the copy to S3 News Data"
+echo "Starting the copy to S3 to trigger automation process"
 aws s3 cp ./poc_data/travel_interactions.csv s3://$bucket/train/travel/interactions.csv
 aws s3 cp ./poc_data/travel_items.csv s3://$bucket/train/travel/items.csv
 aws s3 cp ./poc_data/travel_users.csv s3://$bucket/train/travel/params.json
